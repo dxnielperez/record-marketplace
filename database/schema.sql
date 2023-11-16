7fb5401efcf83386482c532c8dcb8ba7d6989745
Daniel Perez <danielperezf98@gmail.com>
2023-11-16 18:47:02 +0000

    Initial commit

--- database/schema.sql
+++ database/schema.sql
@@ -5,3 +5,68 @@ set client_min_messages to warning;
 drop schema "public" cascade;
 
 create schema "public";
+
+CREATE TABLE "Users" (
+  "userId" serial PRIMARY KEY,
+  "email" text,
+  "password" text,
+  "createdAt" timestamptz
+);
+
+CREATE TABLE "Records" (
+  "recordId" serial PRIMARY KEY,
+  "artist" text,
+  "albumName" text,
+  "genreId" integer,
+  "condition" text,
+  "price" integer,
+  "info" text,
+  "sellerId" integer
+);
+
+CREATE TABLE "Genres" (
+  "genreId" serial PRIMARY KEY,
+  "name" text
+);
+
+CREATE TABLE "Transactions" (
+  "transactionId" serial PRIMARY KEY,
+  "buyerId" integer,
+  "recordId" integer,
+  "totalPrice" integer,
+  "transactionDate" timestamptz
+);
+
+CREATE TABLE "Cart" (
+  "cartId" serial PRIMARY KEY,
+  "userId" integer
+);
+
+CREATE TABLE "CartItems" (
+  "itemsId" serial PRIMARY KEY,
+  "cartId" integer,
+  "recordId" integer,
+  "quantity" integer
+);
+
+CREATE TABLE "Images" (
+  "imageId" serial PRIMARY KEY,
+  "imageUrl" text,
+  "recordId" integer
+);
+
+ALTER TABLE "Records" ADD FOREIGN KEY ("genreId") REFERENCES "Genres" ("genreId");
+
+ALTER TABLE "Records" ADD FOREIGN KEY ("sellerId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Transactions" ADD FOREIGN KEY ("buyerId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "Transactions" ADD FOREIGN KEY ("recordId") REFERENCES "Records" ("recordId");
+
+ALTER TABLE "Cart" ADD FOREIGN KEY ("userId") REFERENCES "Users" ("userId");
+
+ALTER TABLE "CartItems" ADD FOREIGN KEY ("cartId") REFERENCES "Cart" ("cartId");
+
+ALTER TABLE "CartItems" ADD FOREIGN KEY ("recordId") REFERENCES "Records" ("recordId");
+
+ALTER TABLE "Images" ADD FOREIGN KEY ("recordId") REFERENCES "Records" ("recordId");
